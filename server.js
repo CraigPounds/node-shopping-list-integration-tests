00e2a161cef41c2c841f4ecaca3efc1b29169a32
Craig Pounds <nweaver@windstream.net>
2018-08-16 22:10:44 -0500

    prettier

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 });
 
 // when requests come into `/shopping-list` or
-// `/recipes`, we'll route them to the express
+// `/recipes` we'll route them to the express
 // router instances we've imported. Remember,
 // these router instances act as modular, mini-express apps.
 app.use('/shopping-list', shoppingListRouter);
